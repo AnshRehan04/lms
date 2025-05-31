ea8bc376cac5ad0df618f65dcb1ef844a84d3bf8
ansh <anshrehan04@gmail.com>
2025-05-31 16:45:19 +0530

    change server.js

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,8 @@ app.use(express.json()); // to parse json body
 app.use(cookieParser());
 app.use(
     cors({
-        origin: 'http://localhost:5173', // frontend link
+        // origin: 'http://localhost:5173', // frontend link
+        origin: 'https://learnism-frontend.onrender.com', 
         credentials: true
     })
 );
